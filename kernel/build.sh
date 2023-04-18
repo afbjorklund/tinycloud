46f2946ceb236536270aee53b6efbc1a14bb4ada
Anders F Björklund <anders.f.bjorklund@gmail.com>
2023-04-18 20:00:28 +0200

    Add kernel with lima CONFIG_AUDIT enabled

---
+++ kernel/build.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+kernel_version=5.15.10
+
+kernel_volumes="--volume $PWD/kernel_config:/home/tc/kernel_config \
+                --volume $PWD/kernel_defconfig:/home/tc/kernel_defconfig \
+                --volume $PWD/kernel_patches:/home/tc/kernel_patches"
+
+docker container inspect --format '{{.Id}}' tinycore-kernel \
+	|| docker run -d --name=tinycore-kernel $kernel_volumes tinycore-compiletc:13.0-x86_64 sleep 3600
+$(docker inspect --format '{{.State.Running}}' tinycore-kernel) \
+	|| docker start tinycore-kernel
+
+test -r linux-$kernel_version.tar.xz \
+	|| wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$kernel_version.tar.xz
+docker exec tinycore-kernel test -e /home/tc/linux-$kernel_version.tar.xz \
+	|| docker cp linux-$kernel_version.tar.xz tinycore-kernel:/home/tc/linux-$kernel_version.tar.xz
+
+chmod 666 kernel_config
+chmod 666 kernel_defconfig
+
+docker exec -i tinycore-kernel sh -x < compile_kernel
+docker exec -i tinycore-kernel sh -x < package_kernel
+
+docker cp tinycore-kernel:/home/tc/vmlinuz64 .
+docker cp tinycore-kernel:/home/tc/modules .
