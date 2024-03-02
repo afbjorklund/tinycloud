af0a75311108fbfb6d846f81e42b6fd9ff897394
Anders F Björklund <anders.f.bjorklund@gmail.com>
2024-03-02 16:34:10 +0100

    Change lima color to lime green

--- files/color_prompt.sh
+++ files/color_prompt.sh
@@ -5,13 +5,13 @@ esac
 case "$COLORTERM" in
     truecolor) color_prompt=true;;
 esac
-# Lima PS1: set color to lime
+# Lima PS1: set color to lime green
 if [ "$color_prompt" = true ]; then
-    PS1='\[\033[38;2;192;255;0m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='\[\033[38;2;50;205;50m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 elif [ "$color_prompt" = 256 ]; then
-    PS1='\[\033[38;5;154m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='\[\033[38;5;77m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 elif [ "$color_prompt" = yes ]; then
-    PS1='\[\033[01;92m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
     PS1='\u@\h:\w\$ '
 fi
